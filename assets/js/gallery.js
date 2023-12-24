914dd90ce07f0a4b3918f7048f64701a82ebcb34
Sahil Garg <contact.sahilgarg@proton.me>
2023-12-24 09:33:12 +0530

    feat: added js level lazy loading to gallery.

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,24 +1,52 @@
 let galleryContainers = document.querySelectorAll(".gallery_img_container");
 let galleryModal = document.querySelector(".gallery_modal");
 
-galleryContainers.forEach((imgContainer) => {
-	let imgSpan = document.createElement("span");
-	imgSpan.classList.add("gallery_img_span");
-	imgSpan.style.backgroundImage = `url(assets/${
-		imgContainer.querySelector(".gallery_img").src.split("/assets")[1]
-	})`;
-	imgContainer.appendChild(imgSpan);
-	imgContainer.addEventListener("click", () => {
-		let modalImg = document.querySelector(".gallery_modal_img");
-		modalImg.src = `assets/${
-			imgContainer.querySelector(".gallery_img").src.split("/assets")[1]
-		}`;
-		modalImg.setAttribute(
-			"alt",
-			imgContainer.querySelector("h4").innerText
-		);
-		galleryModal.showModal();
+document.addEventListener("DOMContentLoaded", () => {
+	galleryContainers.forEach((imgContainer) => {
+		let imgSpan = document.createElement("span");
+		imgSpan.classList.add("gallery_img_span");
+		if ("IntersectionObserver" in window) {
+			imgSpan.style.backgroundImage =
+				"url(assets/images/gallery/placeholder.webp)";
+		} else {
+			imgSpan.style.backgroundImage = `url(assets/${
+				imgContainer
+					.querySelector(".gallery_img")
+					.src.split("/assets")[1]
+			})`;
+		}
+		imgContainer.appendChild(imgSpan);
+		imgContainer.addEventListener("click", () => {
+			let modalImg = document.querySelector(".gallery_modal_img");
+			modalImg.src = `assets/${
+				imgContainer
+					.querySelector(".gallery_img")
+					.src.split("/assets")[1]
+			}`;
+			modalImg.setAttribute(
+				"alt",
+				imgContainer.querySelector("h4").innerText
+			);
+			galleryModal.showModal();
+		});
 	});
+
+	if ("IntersectionObserver" in window) {
+		let lazyImageLoader = new IntersectionObserver((entries, observer) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					let img = entry.target;
+					img.src = img.dataset.src;
+					entry.target.nextSibling.nextSibling.style.backgroundImage = `url(${img.src})`;
+					img.classList.remove("lazy");
+					lazyImageLoader.unobserve(img);
+				}
+			});
+		});
+		document.querySelectorAll(".lazy").forEach((img) => {
+			lazyImageLoader.observe(img);
+		});
+	}
 });
 
 function closeModal() {
