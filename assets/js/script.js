126512f883a2385c8f81f69f8abc4ccd2d1d438e
Tushar Singh <tushar23123@gmail.com>
2024-03-27 22:27:22 +0530

    acmw images updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,8 @@ $.fn.jQuerySimpleCounter = function( options ) {
 };
 
 $('#number1').jQuerySimpleCounter({end: 583,duration: 3000});
-$('#number2').jQuerySimpleCounter({end: 946,duration: 700});
-$('#number3').jQuerySimpleCounter({end: 81,duration: 3100});
+$('#number2').jQuerySimpleCounter({end: 250,duration: 3100});
+$('#number3').jQuerySimpleCounter({end: 300,duration: 3100});
 $('#number4').jQuerySimpleCounter({end: 16,duration: 3450});
 $('#number5').jQuerySimpleCounter({end: 472,duration: 3300});
 $('#number6').jQuerySimpleCounter({end: 224,duration: 2175});
